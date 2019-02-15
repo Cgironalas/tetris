37b5c676f8aee57c1e82e7b28fa3d68834a316e0
Carlos Giron <cgironalas@gmail.com>
2019-02-15 18:17:43 -0500

    Major refactoring, lines get deleted but come back

--- tetris/src/index.js
+++ tetris/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// A block of the board, considered as a 1x1 within the board.
 function Block(props) {
   return (
     <button className={`block color${props.color}`}>
@@ -10,6 +11,7 @@ function Block(props) {
   );
 }
 
+// A row from the board, 20 will be shown, each one 10 blocks wide.
 class Row extends React.Component {
   render_block(i) {
     return (
@@ -21,6 +23,7 @@ class Row extends React.Component {
     );
   }
 
+  // Renders a board row 10 blocks wide.
   render() {
     return (
       <div className="board-row">
@@ -78,60 +81,143 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // Create the game board as a React Component.
   constructor(props) {
     super(props);
     this.state = {
+      score: 0,
+      next_piece: 'i',
       board: Array(21).fill(
         Array(10).fill(0)
       ),
       moving_piece: {
+        color: 2,
         type: 'i',
         coordinates: [[5, 10],[6,10],[7,10],[8,10]],
-        color: 2,
-      }
+      },
     }
   }
 
-  render_row() {
-    return (
-      <Board
-        rows={this.state.board}
-      />
-    );
+  // Print the game board on console.
+  console_board(board = this.state.board) {
+    let output = "";
+    for (let row of board) {
+      let row_string = row.join(' - ');
+      output = row_string + '\n' + output;
+    }
+    console.log(output);
   }
 
-  update_board(toWhite, toColor, color, coords) {
-    let x, y;
-    let newBoard = [];
-    let newRow = [];
-    let temp;
+  // Get the score multiplier to use based on completed rows in a turn.
+  get_multiplier(rows_completed) {
+    let multiplier;
+    if (rows_completed === 1) {
+      multiplier = 1;
+    } else if (rows_completed === 2) {
+      multiplier = 1.25;
+    } else if (rows_completed === 3) {
+      multiplier = 1.5;
+    } else {
+      multiplier = 2;
+    }
+    return multiplier;
+  }
+
+  // Get the new score based on completed rows in a turn.
+  get_score(rows_completed) {
+    if (rows_completed === 0) {
+      return this.state.score;
+    }
+    else {
+      let multiplier = this.get_multiplier(rows_completed);
+      return this.state.score + ((rows_completed * 500) * multiplier);
+    }
+  }
+
+  update_game(
+    score = this.state.score,
+    next_piece = this.state.next_piece,
+    board = this.state.board,
+    moving_piece = this.state.moving_piece
+  ) {
+    this.setState({
+      score: score,
+      next_piece: next_piece,
+      board: board.slice(),
+      moving_piece: {
+        color: moving_piece.color,
+        type: moving_piece.type,
+        coordinates: moving_piece.coordinates,
+      },
+    });
+    this.forceUpdate();
+    this.console_board();
+  }
+
+  check_finished_rows() {
+    console.log('checking finished rows');
+    let new_board = [];
+    let completed_rows = 0;
+
+    // Check to see if any row is complete (no block as 0)
+    for (let row of this.state.board) {
+      if (row.indexOf(0) > -1) {
+        new_board.push(row);
+      }
+      else {
+        completed_rows++;
+      }
+    }
+    console.log({ lines_completed: completed_rows });
+
+    // If there are completed rows delete them
+    if  (completed_rows > 0) {
+      // Finish filling the board with 0s
+      for (let y = 0; y < completed_rows; y++) {
+        let temp_row = Array(10).fill(0);
+        new_board.push(temp_row);
+      }
+
+      let new_score = this.get_score(completed_rows);
+
+      this.console_board(new_board);
+
+      this.update_game(new_score, undefined, new_board);
+    }
+  }
+
+  update_board(moving_piece, to_white, to_color) {
+    let x, y; // x and y used to traverse the game board.
+    let temp_coords;
+    let temp_row = [];
+    let new_board = [];
+
     for(y = 0; y < 21; y++) {
-      newRow = [];
+      temp_row = [];
+
       for(x = 0; x < 10; x++) {
-        temp = [x, y];
-        if (toColor.hasOwnProperty(temp)) {
-          newRow.push(color);
-        } else {
-          if (toWhite.hasOwnProperty(temp)) {
-            newRow.push(0);
+        temp_coords = [x, y];
+
+        if (to_color.hasOwnProperty(temp_coords)) {
+          temp_row.push(moving_piece.color);
+        }
+        else {
+          if (to_white.hasOwnProperty(temp_coords)) {
+            temp_row.push(0);
           }
           else {
-            newRow.push(this.state.board[y][x]);
+            temp_row.push(this.state.board[y][x]);
           }
         }
       }
-      newBoard.push(newRow);
+
+      new_board.push(temp_row);
     }
 
-    this.setState({
-      board: newBoard.slice(),
-      moving_piece: {
-        coordinates: coords,
-        color: color
-      },
-    });
+    this.update_game(undefined, undefined, new_board, moving_piece);
   }
 
+  // default settings to create any new piece
   create_piece(type) {
     switch(type) {
       case 'o':
@@ -189,14 +275,15 @@ class Game extends React.Component {
 
   get_object_from_piece(piece) {
     let obj = {};
-    let i;
-    for (i = 0; i < piece.coordinates.length; i++) {
-      obj[piece.coordinates[i]] = i;
+    for (let coordinates of piece.coordinates) {
+      obj[coordinates] = 0;
     }
     return obj;
   }
 
   generate_random_piece() {
+    this.check_finished_rows();
+    this.console_board();
     let piece_types = ['o','i','t','l','j','s','z'];
     let current_type_index = piece_types.indexOf(this.state.moving_piece.type);
     if (current_type_index > -1) {
@@ -204,50 +291,35 @@ class Game extends React.Component {
     }
 
     let type = piece_types[Math.floor(Math.random()*piece_types.length)];
-    let newPiece = this.create_piece(type);
-    console.log(newPiece);
+    let new_piece = this.create_piece(type);
+    console.log(new_piece);
 
-    let toColor = this.get_object_from_piece(newPiece);
-    let toWhite = this.get_object_from_piece(this.state.moving_piece);
-
-    this.update_board({}, {}, newPiece.color, newPiece.coordinates);
-    //this.update_board(toWhite, toColor, newPiece.color, newPiece.coordinates);
-    //this.move_piece('down');
-  }
-
-  consoleBoard() {
-    let i;
-    let output = "";
-    for (i = 0; i < this.state.board.length; i++) {
-      let row = this.state.board[i].join(' - ');
-      output += row + '\n';
-    }
-    console.log(output);
+    this.update_game(undefined, undefined, undefined, new_piece);
   }
 
   move_piece(direction) {
-    let i;
-    let coords = this.state.moving_piece.coordinates;
-    let color = this.state.moving_piece.color;
-    let coordsObj = this.get_object_from_piece(this.state.moving_piece);
-    let toWhite = {};
-    let toColor = {};
-    let newCoords = [];
     let x, y;
+    let to_white = {};
+    let to_color = {};
+    let new_coords = [];
     let generate_new = false;
 
-    for (i = 0; i < coords.length; i++) {
-      x = coords[i][0];
-      y = coords[i][1];
+    let color = this.state.moving_piece.color;
+    let type = this.state.moving_piece.color;
+    let coords = this.state.moving_piece.coordinates;
+    let coords_obj = this.get_object_from_piece(this.state.moving_piece);
+
+    for (let pair of coords) {
+      x = pair[0];
+      y = pair[1];
 
       switch(direction) {
         case 'left':
           if (x > 0) {
-            //console.log('move left');
-            toWhite[[x, y]] = i;
+            to_white[[x, y]] = 0;
             x--;
-            toColor[[x, y]] = i;
-            newCoords.push([x,y]);
+            to_color[[x, y]] = 0;
+            new_coords.push([x,y]);
           }
           else {
             return;
@@ -256,11 +328,10 @@ class Game extends React.Component {
 
         case 'right':
           if (x < 9) {
-            //console.log('move right');
-            toWhite[[x, y]] = i;
+            to_white[[x, y]] = 0;
             x++;
-            toColor[[x, y]] = i;
-            newCoords.push([x,y]);
+            to_color[[x, y]] = 0;
+            new_coords.push([x,y]);
           }
           else {
             return;
@@ -268,22 +339,18 @@ class Game extends React.Component {
           break;
 
         case 'drop':
-          //console.log('drop');
-          toWhite[[x, y]] = i;
-          toColor[[x, 0]] = i;
-          newCoords.push([x,y]);
+          to_white[[x, y]] = 0;
+          to_color[[x, y]] = 0;
+          new_coords.push([x,y]);
           break;
 
         default:
           if (y > 0) {
-            //console.log('move down');
-            toWhite[[x, y]] = i;
+            to_white[[x, y]] = 0;
             y--;
-            toColor[[x, y]] = i;
-            newCoords.push([x,y]);
-            //console.log({ x: x, y: y });
-            //this.consoleBoard();
-            if (this.state.board[y][x] !== 0 && !coordsObj.hasOwnProperty([x, y])) {
+            to_color[[x, y]] = 0;
+            new_coords.push([x,y]);
+            if (this.state.board[y][x] !== 0 && !coords_obj.hasOwnProperty([x, y])) {
               generate_new = true;
               break;
             }
@@ -302,78 +369,74 @@ class Game extends React.Component {
       this.generate_random_piece();
     }
     else {
-      this.update_board(toWhite, toColor, color, newCoords);
+      this.update_board({color: color, type: type, coordinates: new_coords}, to_white, to_color);
     }
-    this.consoleBoard();
   }
 
   handleKeyPress = (event) => {
-    //console.log(event.key);
+    //console.log("key pressed: ");
+    //console.log({charCode: event.charCode, key: event.key, keyCode: event.keyCode});
 
     // DROP
-    if (event.key === 'Enter') {
+    if (event.keyCode === 13) {
       //console.log('DROP');
       this.move_piece('drop');
       return;
     }
 
     // Rotations
-    if (event.key === 'r' || event.key === ' ') {
-      console.log('ROTATE RIGHT');
+    if (event.keyCode === 82 || event.keyCode === 32) {
+      //console.log('ROTATE RIGHT');
       this.generate_random_piece();
       return;
     }
-    if (event.key === 'R') {
-      console.log('ROTATE LEFT');
+    if (event.keyCode === 88) {
+      //console.log('ROTATE LEFT');
       return;
     }
 
-    var lowerKey = event.key.toLowerCase()
-
     // Pause
-    if (lowerKey === 'p') {
-      console.log('PAUSE');
+    if (event.keyCode === 80) {
+      //console.log('PAUSE');
       return;
     }
 
     // Movement
-    if (lowerKey === 'a') {
+    if (event.keyCode === 65 || event.keyCode === 37) {
       //console.log('MOVE LEFT');
       this.move_piece('left');
       return;
     }
-    if (lowerKey === 'd' || lowerKey === 'e') {
+    if (event.keyCode === 68 || event.keyCode === 69 || event.keyCode === 39) {
       //console.log('MOVE RIGHT');
       this.move_piece('right');
       return;
     }
-    if (lowerKey === 's' || lowerKey === 'o') {
+    if (event.keyCode === 83 || event.keyCode === 79 || event.keyCode === 40) {
       //console.log('MOVE BOTTOM');
       this.move_piece('bottom');
       return;
     }
 
     // Hold piece
-    if (lowerKey === 'h' || lowerKey === 'j') {
-      console.log('HOLD');
+    if (event.keyCode === 72 || event.keycode === 74) {
+      //console.log('HOLD');
       return;
     }
   }
 
   render() {
-    const score = 0;
-    const nextPiece = 0;
     const leaderboard = "";
     return (
-      <div className="game" onKeyPress={this.handleKeyPress}>
+      <div className="game" onKeyDown={this.handleKeyPress}>
         <div className="game-board">
           <Board rows={this.state.board} />
         </div>
         <div className="game-info">
           <div>Score</div>
-          <div>{score}</div>
+          <div>{this.state.score}</div>
           <div>Next Piece</div>
-          <div>{nextPiece}</div>
+          <div>{this.state.nextPiece}</div>
           <div>Leaderboard</div>
           <div>{leaderboard}</div>
         </div>
